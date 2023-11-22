3fcfad8cefc50dea94c6f4affd99f33969859a2d
iamarabarrios <iamarabarrios457@gmail.com>
2023-11-22 16:28:23 -0300

    return

--- models/city.py
+++ models/city.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 import os
-
 STORE = os.getenv('HBNB_TYPE_STORAGE')
 
 
