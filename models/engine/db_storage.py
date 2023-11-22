d74e0ff434dc15c991206dcb8fee6e15ce2b8d9e
juandi131 <juandigarcia3@gmail.com>
2023-11-22 06:59:19 -0800

    12

---
+++ models/engine/db_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" db storage """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base, BaseModel
+import os
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+USER = os.getenv('HBNB_MYSQL_USER')
+PASS = os.getenv('HBNB_MYSQL_PWD')
+HOST = os.getenv('HBNB_MYSQL_HOST')
+DATA = os.getenv('HBNB_MYSQL_DB')
+ENV = os.getenv('HBNB_ENV')
+
+
+class DBStorage:
+    """ db storage """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine =\
+            create_engine(f"mysql+mysqldb://{USER}:{PASS}@{HOST}/{DATA}",
+                          pool_pre_ping=True)
+        if ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ returns a dict """
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        dic = {}
+        if cls is not None:
+            for obj in self.__session.query(classes[cls]).all():
+                dic[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        else:
+            for clas in classes.values():
+                for obj in self.__session.query(clas).all():
+                    dic[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        return dic
+
+    def new(self, obj):
+        """ add object to the current database """
+        self.__session.add(obj)
+        self.__session.commit()
+
+    def reload(self):
+        """ creates all tables """
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
+
+    def save(self):
+        """ commit all changes """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database """
+        if obj is not None:
+            self.__session.delete(obj)
+            self.save()
+
+    def close(self):
+        """ deserialize the JSON file """
+        self.__session.remove()
