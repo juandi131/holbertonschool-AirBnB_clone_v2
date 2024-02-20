59872b52c79b68fa8e563897e4fb963a83d6d08c
juandi131 <juandigarcia3@gmail.com>
2024-02-19 16:23:59 -0800

    12

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-""" start"""
-
+""""""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def lists():
-    return render_template('7-states_list.html', data=storage.all(State))
+def teardown_appcontext(exception=None):
+
+    storage.close()
 
 
+app.teardown_appcontext(teardown_appcontext)
 
-if __name__ == "__main__":
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+
+    states = storage.all(State)
+    sorted_states = {state.id: state for state in
+                     sorted(states.values(), key=lambda x: x.name)}
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
